d9b7525ad2841a459500c39065f2b3c2442c9b09
Hendi Santika <hendisantika@gmail.com>
2019-01-21 03:53:17 +0700

    #1 - first commit

---
+++ src/main/java/com/kominfo/halaqohit/springbootrestapimongodb/SpringBootRestApiMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.kominfo.halaqohit.springbootrestapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiMongodbApplication.class, args);
+    }
+
+}
+
